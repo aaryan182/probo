2cb4ef57069aa71d307dacb5ef59c2069fc6174d
aaryan <aaryanbajaj385@gmail.com>
2024-10-17 14:03:48 +0530

    completed v0

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const routes = require('./routes/index.route');
+const express = require("express");
+const routes = require("./routes/index.route");
 const app = express();
 const port = 3000;
-const { initialiseDummyData } = require('./controllers/index.controller');
+const { initialiseDummyData } = require("./controllers/index.controller");
 
 app.use(express.json());
 
 initialiseDummyData();
 
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(port, () => {
-    console.log(`Options trading app listening at http://localhost:${port}`);
+  console.log(`Options trading app listening at http://localhost:${port}`);
 });
